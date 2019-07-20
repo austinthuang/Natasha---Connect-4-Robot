c365ea5f1b72eab55b59e7823cb3522fa8e5658a
tiesmalicious <akira.tiesma@gmail.com>
2019-07-20 16:17:43 -0400

    converted array logic back to 2D, added check win functions, and added slight delay to dispensing

--- test.c
+++ test.c
@@ -12,7 +12,9 @@ const float COL_3 = 19.08;
 const float COL_4 = 22.85;
 const float COL_5 = 26.82;
 const float COL_6 = 30.22;
-const float COL_7 = 33.9;
+const float COL_7 = 33.92;
+
+int gameBoard[BOARD_ROWS][BOARD_COLS];
 
 void Dispense() //Dispenses a chip
 {
@@ -40,27 +42,27 @@ void recallHorSensors() //resets the motor encoder for the motor running in the 
 	nMotorEncoder[motorA] = 0;
 }
 
-bool rowCheck(int *gameBoard, int slot, int token)
+bool rowCheck(int row, int col, int token)
 {
-	if (slot != token)
+	if (gameBoard[row][col] != token)
 		return false;
 
 	int counter = 1;
 	bool cont = true;
 
 	//go right
-	for (int i = slot + 1; i < 21 && cont == true; i++)
+	for (int i = col + 1; i < BOARD_COLS && cont == true; i++)
 	{
-		if (gameBoard[i] == token)
+		if (gameBoard[row][i] == token)
 			counter++;
 		else
 			cont = false;
 	}
 	cont = true;
 	//go left
-	for (int i = slot - 1; i > 13 && cont == true; i++)
+	for (int i = col - 1; i >=0 && cont == true; i++)
 	{
-		if (gameBoard[i] == token)
+		if (gameBoard[row][i] == token)
 			counter++;
 		else
 			cont = false;
@@ -68,7 +70,101 @@ bool rowCheck(int *gameBoard, int slot, int token)
 	return counter > 3;
 }
 
-void test(float *columns )
+bool colCheck (int row, int col, int token)
+{
+	if (gameBoard[row][col] != token)
+		return false;
+
+	int counter = 1;
+	bool cont  = true;
+	//go down
+	for (int i = row + 1; i < BOARD_ROWS && cont == true; i++)
+	{
+		if (gameBoard[i][col] == token)
+			counter++;
+		else
+			cont = false;
+	}
+	cont = true;
+	//go up
+	for (int i = row - 1; i >=0 && cont == true; i++)
+	{
+		if (gameBoard[i][col] == token)
+			counter++;
+		else
+			cont = false;
+	}
+	return counter > 3;
+}
+
+bool diagonalCheck(int row, int col, int token)
+{
+	if (gameBoard[row][col] != token)
+		return false;
+
+	int counter = 1;
+
+	//bottom left to top right
+	int trow = row - 1, tcol = col + 1;
+	bool cont = true;
+	while (trow >= 0 && tcol < BOARD_COLS && cont == true)
+	{
+		if (gameBoard[trow][tcol] == token)
+			counter++;
+		else
+			cont = false;
+		trow--;
+		tcol++;
+	}
+
+	trow = row + 1;
+	tcol = col - 1;
+	cont = true;
+	//top right to bottom left
+	while (trow < BOARD_ROWS && tcol >= 0 && cont == true)
+	{
+		if (gameBoard[trow][tcol] == token)
+			counter++;
+		else
+			cont = false;
+		trow++;
+		tcol--;
+	}
+	if (counter > 3)
+		return true;
+
+	//top left to bottom right
+	counter = 1;
+	trow = row + 1;
+	tcol = col + 1;
+	cont = true;
+	while (trow < BOARD_ROWS && tcol < BOARD_COLS && cont == true)
+	{
+		if (gameBoard[trow][tcol] == token)
+			counter++;
+		else
+			cont = false;
+		trow++;
+		tcol++;
+	}
+
+	//bottom right to top left
+	trow = row - 1;
+	tcol = col - 1;
+	cont = true;
+	while (trow >= 0 && tcol >= 0 && cont == true)
+	{
+		if (gameBoard[trow][tcol] == token)
+			counter++;
+		else
+			cont = false;
+		trow--;
+		tcol--;
+	}
+	return counter > 3;
+}
+
+void test(float *columns ) //Test hardware
 {
 	for(int i = 0; i < 7; i++)
 	{
@@ -77,6 +173,7 @@ void test(float *columns )
 		motor[motorA]= 25;
 		while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*(columns[i]))){}
 		motor[motorA] = 0;
+		wait1Msec(50);
 		Dispense();
 	}
 }
@@ -93,14 +190,10 @@ task main()
 	float columns[7] = {COL_1, COL_2, COL_3, COL_4, COL_5, COL_6, COL_7};
 
 	//creates virtual gameboard and fills it with all empty values
-	int gameBoard[BOARD_ROWS * BOARD_COLS];
-	for(int i = 0; i < BOARD_ROWS; i++)
-	{
-		gameBoard[i] = 0;
-	}
 
 	while(!getButtonPress(buttonAny)){}
 	test(columns);
+	recallHorSensors();
 
 	//added some stuff
 
