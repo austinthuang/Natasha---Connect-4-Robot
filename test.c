7454712bb8cff2c08c612c3743e2191191cf6010
tiesmalicious <akira.tiesma@gmail.com>
2019-07-20 18:28:48 -0400

    Added scanning function

--- test.c
+++ test.c
@@ -1,11 +1,6 @@
-const float HOR_MID_DIST = 3.5;
-const float CALIBRATE_BOARD = 9.0;
+//const float CALIBRATE_BOARD = 9.0;
 const int BOARD_ROWS = 6;
 const int BOARD_COLS = 7;
-const int SLOT_E = 0;
-const int SLOT_R = 5;
-const int SLOT_Y = 4;
-const int BLUE = 2;
 const float COL_1 = 11.58;
 const float COL_2 = 15.38;
 const float COL_3 = 19.08;
@@ -13,10 +8,11 @@ const float COL_4 = 22.85;
 const float COL_5 = 26.82;
 const float COL_6 = 30.5;
 const float COL_7 = 34.5;
+const float CENT_CENT = 3.42;
 
 int gameBoard[BOARD_ROWS][BOARD_COLS];
 
-void Dispense() //Dispenses a chip
+void dispense() //Dispenses a chip
 {
 	motor[motorC] = 20;
 	wait1Msec(350);
@@ -28,7 +24,7 @@ void Dispense() //Dispenses a chip
 
 void recallVerSensors() //resets the motor encoder for the motor running in the vertical xis
 {
-	motor[motorB]= -100;
+	motor[motorB]= -50;
 	while(SensorValue[S1] == 0){}
 	motor[motorB] = 0;
 	nMotorEncoder[motorB] = 0;
@@ -164,7 +160,7 @@ bool diagonalCheck(int row, int col, int token)
 	return counter > 3;
 }
 
-void test(float *columns ) //Test hardware
+void test(float *columns, int color) //Test hardware
 {
 	for(int i = 0; i < 7; i++)
 	{
@@ -173,9 +169,40 @@ void test(float *columns ) //Test hardware
 		motor[motorA]= 25;
 		while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*(columns[i]))){}
 		motor[motorA] = 0;
+		displayBigTextLine(3, "%d ",color);
+		wait1Msec(500);
+		eraseDisplay();
 		wait1Msec(50);
-		Dispense();
+		dispense();
+
 	}
+}
+
+int scanCol(int colNum)
+{
+	for(int i = 0; i < BOARD_ROWS; i++)
+	{
+		wait1Msec(50);
+		if (SensorValue[S3] == (int)colorYellow)
+		{
+			gameBoard[i][colNum] = (int)colorYellow;
+			return i;
+		}
+		else if(i < (BOARD_ROWS-2))//checks to see if board already has next index saved
+		{
+			if (gameBoard[i+1][colNum] != 0)
+				return -1; // dummy place hold value to exit loop
+		}
+		if (i<BOARD_ROWS-1)
+		{
+			nMotorEncoder[motorB] = 0;
+			motor[motorB] = 15;
+			while (nMotorEncoder[motorB] < ((180/(PI*1.1))*(CENT_CENT))){}
+			motor[motorB] = 0;
+		}
+	}
+	recallVerSensors();
+	return -1;
 }
 
 
@@ -191,9 +218,27 @@ task main()
 
 	//creates virtual gameboard and fills it with all empty values
 
-	while(!getButtonPress(buttonAny)){}
-	test(columns);
+	/*while(!getButtonPress(buttonAny)){}
+	test(columns, SensorValue[S3]);
+	recallHorSensors();*/
+
+	/*while(!getButtonPress(buttonBack)){
+	displayBigTextLine(3,"%d ", SensorValue[S3]);
+	wait1Msec(5000);
+	eraseDisplay();
+	}*/
+	recallVerSensors();
 	recallHorSensors();
+	for(int i = 0; i < BOARD_COLS; i++)
+	{
+		wait1Msec(500);
+		motor[motorA]= 25;
+		while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*(columns[i]))){}
+		motor[motorA] = 0;
+		scanCol(i);
+		recallVerSensors();
+	}
+
 
 	//added some stuff
 
