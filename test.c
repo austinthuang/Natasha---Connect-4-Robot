3236cc29bc4f01feb4938f9d1a38a3d791ba163a
tiesmalicious <akira.tiesma@gmail.com>
2019-07-28 09:59:13 -0400

    Depth is now 5, the waits on dispense is changed to 700ms, comments are added throughout the entire code, and unnecessary parts deleted

--- test.c
+++ test.c
@@ -4,21 +4,22 @@ const float COL_1 = 11.58;
 const float COL_2 = 15.38;
 const float COL_3 = 19.08;
 const float COL_4 = 22.85;
-const float COL_5 = 26.82;
+const float COL_5 = 26.92;
 const float COL_6 = 30.7;
 const float COL_7 = 34.9;
 const float CENT_CENT = 3.42;
+const float WHEEL_RAD = 2.1575;
 
 int gameBoard[BOARD_ROWS][BOARD_COLS];
-int dummyBoard[BOARD_ROWS][BOARD_COLS];
+int dummyBoard[BOARD_ROWS][BOARD_COLS];			//dummy placeholder to keep current game state before robot turn
 int scores[BOARD_COLS];
 
 void dispense() //Dispenses a chip
 {
-	motor[motorC] = 20;
-	wait1Msec(350);
+	motor[motorC] = 20; // motor C is dispenser
+	wait1Msec(700);
 	motor[motorC] = -20;
-	wait1Msec(1000);
+	wait1Msec(700);
 	motor[motorC] = 0;
 }
 
@@ -38,14 +39,15 @@ void recallHorSensors() //resets the motor encoder for the motor running in the 
 	nMotorEncoder[motorA] = 0;
 }
 
-int checkBoard()
+int checkBoard()	//Check if the board is there
 {
 	recallVerSensors();
 	recallHorSensors();
 	nMotorEncoder[motorA]= 0;
 	motor[motorA] = 25;
-	while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*COL_7)){
-		if (SensorValue[S3] == (int)colorBlue)
+	while (nMotorEncoder[motorA] < ((180/(PI*WHEEL_RAD))*COL_7))
+	{
+		if (SensorValue[S3] == (int)colorBlue) //if it sees the board,reset position
 		{
 			recallHorSensors();
 			return 1;
@@ -53,14 +55,14 @@ int checkBoard()
 	}
 	motor[motorA]=0;
 	setSoundVolume(100);
-	playSoundFile("Error alarm");
+	playSoundFile("Error alarm");		//If not seen, make error sound
 	sleep(1000);
 	recallHorSensors();
-	return -1;
+	return -1;											// End program
 
 }
 
-bool rowCheck(int row, int col, int token)
+bool rowCheck(int row, int col, int token) 		//Check if there is a horizontal win
 {
 	if (gameBoard[row][col] != token)
 		return false;
@@ -91,7 +93,7 @@ bool rowCheck(int row, int col, int token)
 		return false;
 }
 
-bool colCheck (int row, int col, int token)
+bool colCheck (int row, int col, int token)		//Check for vertical win
 {
 	if (gameBoard[row][col] != token)
 		return false;
@@ -121,7 +123,7 @@ bool colCheck (int row, int col, int token)
 		return false;
 }
 
-bool diagonalCheck(int row, int col, int token)
+bool diagonalCheck(int row, int col, int token)		//Check for diagonal win
 {
 	if (gameBoard[row][col] != token)
 		return false;
@@ -191,7 +193,7 @@ bool diagonalCheck(int row, int col, int token)
 		return false;
 }
 
-bool checkFullColumn(int col)
+bool checkFullColumn(int col)		//Check if there is a column fully filled
 {
 	for (int i = 0; i < BOARD_ROWS; i++)
 	{
@@ -201,7 +203,7 @@ bool checkFullColumn(int col)
 	return true;
 }
 
-int scanCol(int colNum)
+int scanCol(int colNum)			//Scanning for a player's token
 {
 	if (!checkFullColumn(colNum))
 	{
@@ -210,7 +212,7 @@ int scanCol(int colNum)
 			wait1Msec(50);
 			if (SensorValue[S3] == (int)colorYellow)
 			{
-				gameBoard[i][colNum] = (int)colorYellow;
+				gameBoard[i][colNum] = (int)colorYellow;		//update game board state
 				return i;
 			}
 			else if(i < (BOARD_ROWS-1))//checks to see if board already has next index saved
@@ -222,7 +224,7 @@ int scanCol(int colNum)
 			{
 				nMotorEncoder[motorB] = 0;
 				motor[motorB] = 15;
-				while (nMotorEncoder[motorB] < ((180/(PI*1.1))*(CENT_CENT))){}
+				while (nMotorEncoder[motorB] < ((180/(PI*1.1))*(CENT_CENT))){}	//Drives colour sensor down
 				motor[motorB] = 0;
 			}
 		}
@@ -231,19 +233,19 @@ int scanCol(int colNum)
 	return -1;
 }
 
-int dropPiece (int col, int token)
+int dropPiece (int col, int token)		//Updates gameboard
 {
 	for(int i = 0; i < BOARD_ROWS; i++)
 	{
 		if (i != 5)
 		{
-			if (gameBoard[i+1][col] != 0)
+			if (gameBoard[i+1][col] != 0)		//Place token if there is already a token below it
 			{
 				gameBoard[i][col] = token;
 				return i;
 			}
 		}
-		else if(gameBoard[5][col] == 0)
+		else if(gameBoard[5][col] == 0)		//place token on bottom if bottom is empty
 		{
 			gameBoard[5][col] = token;
 			return 5;
@@ -253,7 +255,7 @@ int dropPiece (int col, int token)
 }
 
 
-void displayWin(int condition)
+void displayWin(int condition)		//Displays on EV3 screen that someone won, and play music
 {
 	setSoundVolume(100);
 	eraseDisplay();
@@ -278,7 +280,7 @@ void displayWin(int condition)
 
 }
 
-int checkWin(int token)
+int checkWin(int token)			//Puts all win checks into one
 {
 	for(int i = 0; i < BOARD_ROWS; i++)
 	{
@@ -306,10 +308,10 @@ int checkWin(int token)
 				return 0;
 		}
 	}
-	return 3;
+	return 3;			//Tie
 }
 
-bool checkFullBoard()
+bool checkFullBoard()		//Check if the entire board is filled
 {
 	for (int i = 0; i < BOARD_ROWS; i++)
 	{
@@ -322,7 +324,7 @@ bool checkFullBoard()
 	return true;
 }
 
-int max(int value1, int value2)
+int max(int value1, int value2) //Find maximum between two values
 {
 	if (value1 > value2)
 		return value1;
@@ -332,7 +334,7 @@ int max(int value1, int value2)
 		return value1;
 }
 
-int min(int value1, int value2)
+int min(int value1, int value2)	//Find minimum between two values
 {
 	if (value1 < value2)
 		return value1;
@@ -342,7 +344,7 @@ int min(int value1, int value2)
 		return value1;
 }
 
-void removeToken(int col)
+void removeToken(int col)		//Remove a token from the game board
 {
 	bool top = false;
 	for(int i = 0; i < BOARD_ROWS && top == false; i++)
@@ -355,13 +357,19 @@ void removeToken(int col)
 	}
 }
 
-int miniMax (int depth, bool ai, int alpha, int beta) //
+//Robot's decision making algorithm
+//Robot sees a number of turns in the future based on depth value
+//Robot places tokens in all combinations and decides what results in its win, or the player's win
+//Returns int score value for that column in scores array
+int miniMax (int depth, bool ai, int alpha, int beta)
 {
 	if (depth == 0 || checkFullBoard())
 		return 0;
 
 	if (checkWin((int)colorRed))
+	{
 		return depth;
+	}
 	if (checkWin((int)colorYellow))
 		return -depth;
 
@@ -374,11 +382,11 @@ int miniMax (int depth, bool ai, int alpha, int beta) //
 		if (!checkFullColumn(i))
 		{
 			if (ai)
-				dropPiece(i,(int)colorRed); // Depth 4: row = 4,ai depth 2: row = 5,ai
+				dropPiece(i,(int)colorRed); //Drop a robot token into anywhere it can be placed
 			else
-				dropPiece(i,(int)colorYellow); //Depth 3: row = 3,!ai depth 1: row = 5,!ai
+				dropPiece(i,(int)colorYellow); //Drip a player token into anywhere it can be placed
 
-			int prevVal = miniMax(depth - 1, !ai, alpha, beta); //
+			int prevVal = miniMax(depth - 1, !ai, alpha, beta); //Recursively called until depth = 0
 
 			if (ai)
 			{
@@ -390,7 +398,10 @@ int miniMax (int depth, bool ai, int alpha, int beta) //
 				bestVal = min(prevVal, bestVal); //0,0,0,0,0,0,0
 				beta = min(beta, bestVal);
 			}
-			removeToken(i);
+			removeToken(i);		//Remove the token added for decision making
+
+			//Alpha-Beta Pruning - as soon as this statement is true, it breaks out of the recursive funcition
+			//Used for optimization - decrease number of combinations the robot has to analyze
 			if (alpha >= beta)
 				break;
 		}
@@ -398,9 +409,9 @@ int miniMax (int depth, bool ai, int alpha, int beta) //
 	return bestVal;
 }
 
-int evaluateMedian(int count, int high, int *array)
+int evaluateMedian(int count, int high, int *array) //Find the median of all columns with the 'best' move
 {
-	if (count == 1 || count == 2)
+	if (count == 1 || count == 2) //If only 1 or 2 best columns, pick the first one in array
 	{
 		for (int i = 0; i < 7; i++)
 		{
@@ -410,7 +421,7 @@ int evaluateMedian(int count, int high, int *array)
 	}
 	else
 	{
-		for (int i =0; i < 7; i++)
+		for (int i =0; i < 7; i++)		//Look for median from the left
 		{
 			if (array[i] != -10)
 			{
@@ -418,7 +429,7 @@ int evaluateMedian(int count, int high, int *array)
 				i = 7;
 			}
 		}
-		for (int j = 6; j >= 0; j--)
+		for (int j = 6; j >= 0; j--)		//Look for median from the right
 		{
 			if (array[j] != -10)
 			{
@@ -426,13 +437,13 @@ int evaluateMedian(int count, int high, int *array)
 				j = -1;
 			}
 		}
-		return evaluateMedian(count-2,high, array);
+		return evaluateMedian(count-2,high, array);			//Called recursively until one value is left
 	}
 }
 
-int median(int *array)
+int median(int *array)		//Find the highest scores from the array
 {
-	int dummy[7] = {-10,-10,-10,-10,-10,-10,-10};
+	int dummy[7] = {-10,-10,-10,-10,-10,-10,-10};			//dummy to save what is in the array
 	int count = 0;
 
 	int high = -10000000;
@@ -445,17 +456,17 @@ int median(int *array)
 	}
 	for (int j = 0; j < 7; j++)
 	{
-		if (array[j] == high)
+		if (array[j] == high)		//Count how many columns have the highest score
 		{
 			dummy[j] = array[j];
 			count++;
 		}
 	}
-	return evaluateMedian(count, high, dummy);
+	return evaluateMedian(count, high, dummy);		//Call evaluateMedian to find median
 }
 
 
-void driveAndDispense(float *columns, int &row, int &col) //Test hardware
+void driveAndDispense(float *columns, int &row, int &col) //Function for Robot's Turn
 {
 	for (int i = 0; i < BOARD_ROWS; i++) // Fill Dummy array
 	{
@@ -470,50 +481,64 @@ void driveAndDispense(float *columns, int &row, int &col) //Test hardware
 		if (!checkFullColumn(i))
 		{
 			row = dropPiece(i,(int)colorRed);
-			scores[i] = miniMax(4, false, -10000, 10000); //
+			scores[i] = miniMax(5, false, -10000, 10000); //Assign score for each column
 			removeToken(i);
 		}
 		else
-			scores[i] = -6;
+			scores[i] = -6;		//a full column has the lowest score
 	}
-	setSoundVolume(100);
+	setSoundVolume(100);		//Make sound to signify that it decided
 	playSoundFile("Ready");
+	bool win = false;
+	//Choose the column to put it in based on the scores
 	col = median(scores);
 
-	displayBigTextLine(3, "%d %d %d %d %d %d %d", scores[0],scores[1],scores[2],scores[3],scores[4],scores[5],scores[6]);
-	displayBigTextLine(5, "%d", col);
+	displayTextLine(3, "%d %d %d %d %d %d %d", scores[0],scores[1],scores[2],
+	scores[3],scores[4],scores[5],scores[6]); //Display scores of each column
+	displayTextLine(5, "%d", col);
 	wait1Msec(5000);
-	motor[motorA]= 25;
-	//col = 0;
-	while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*(columns[col]))){}
+	for(int i = 0; i < BOARD_COLS && win == false; i++) //Make a sound when it is making its winning move
+	{
+		if (scores[i] == 4)
+		{
+			setSoundVolume(100);
+			playSoundFile("supaHOT");
+			sleep(7000);
+			win = true;
+		}
+	}
+	motor[motorA]= 25;		//Move to column
+	while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*(columns[col]+0.4))){}
 	motor[motorA] = 0;
 	wait1Msec(500);
 	eraseDisplay();
 	wait1Msec(50);
-	dispense();
+	dispense();		//Dispense token
 
-	for (int i = 0; i < BOARD_ROWS; i++) // Fill Dummy array
+	for (int i = 0; i < BOARD_ROWS; i++) // Bring gameboard back to current state
 	{
 		for (int j = 0; j < BOARD_COLS; j++)
 		{
 			gameBoard[i][j] = dummyBoard[i][j];
 		}
 	}
-	row = dropPiece(col, (int)colorRed);
+	row = dropPiece(col, (int)colorRed);	// Add new robot token
 }
 
-void endScreen()
+void endScreen()		//Screen after game ended
 {
+	eraseDisplay();
+	for(int i = 0;i < BOARD_ROWS; i++) //Display final game state
+	{
+		displayBigTextLine(2 * i, "%d %d %d %d %d %d %d", gameBoard[i][0],gameBoard[i][1],
+		gameBoard[i][2],gameBoard[i][3],gameBoard[i][4],gameBoard[i][5],
+		gameBoard[i][6]);
+	}
+	wait1Msec(4000);
 	eraseDisplay();
 	displayBigTextLine(5, "PLAY AGAIN?");
 	displayBigTextLine(8, "DOWN FOR NO");
 	displayBigTextLine(10, "ANY FOR YES");
-	wait1Msec(2000);
-	eraseDisplay();
-	for(int i = 0;i < BOARD_ROWS; i++)
-	{
-		displayBigTextLine(2 * i, "%d %d %d %d %d %d %d", gameBoard[i][0],gameBoard[i][1],gameBoard[i][2],gameBoard[i][3],gameBoard[i][4],gameBoard[i][5],gameBoard[i][6]);
-	}
 }
 
 task main()
@@ -527,32 +552,32 @@ task main()
 	SensorType[S4] = sensorEV3_Touch; //Next turn sensor
 	float columns[7] = {COL_1, COL_2, COL_3, COL_4, COL_5, COL_6, COL_7};
 
-	if (checkBoard() == 1)
+	if (checkBoard() == 1) //if the board is present
 	{
 		int win = 0;
-		bool playAgain = true;//0= no win, 1 = player win, 2 = robot win, 3 = tie
-		while(playAgain == true)
+		bool playAgain = true;
+		while(playAgain == true) 	// Game begins here
 		{
 			int lastPieceRow = 0, lastPieceCol = 0;
-			for(int i = 0; i < BOARD_ROWS; i++)
+			for(int i = 0; i < BOARD_ROWS; i++) 	// Reset Game state
 			{
 				for(int j = 0; j < BOARD_COLS; j++)
 				{
 					gameBoard[i][j] = 0;
 				}
 			}
-			while(win == 0)
+			while(win == 0)		//0= no win, 1 = player win, 2 = robot win, 3 = tie
 			{
 				//wait for player turn
-				for(int i = 0;i < BOARD_ROWS; i++) // display array to screen
+				for(int i = 0;i < BOARD_ROWS; i++) // display game state to screen
 				{
 					displayBigTextLine(2 * i, "%d %d %d %d %d %d %d", gameBoard[i][0],gameBoard[i][1],gameBoard[i][2],gameBoard[i][3],gameBoard[i][4],gameBoard[i][5],gameBoard[i][6]);
 				}
-				time1[T1] = 0;
+				time1[T1] = 0; 	//reset timer
 
 				while(!SensorValue[S4])
 				{
-					if (time1[T1] % 20000 == 0)
+					if (time1[T1] % 20000 == 0) // Alert player every 20 seconds
 					{
 						setSoundVolume(100);
 						playSoundFile("Ready");
@@ -560,14 +585,14 @@ task main()
 					}
 				}
 				eraseDisplay();
-				for(int i = 0; i < BOARD_COLS; i++)
+				for(int i = 0; i < BOARD_COLS; i++)  	//Scan the board for new player token
 				{
 					motor[motorA]= 20;
 					while (nMotorEncoder[motorA] < ((180/(PI*2.1575))*(columns[i]))){}
 					motor[motorA] = 0;
 					lastPieceRow = scanCol(i);
 					lastPieceCol = i;
-					if (SensorValue[S4] == 1)
+					if (SensorValue[S4] == 1)				//If touch sensor is pressed, reset and restart scan
 					{
 						i = -1;
 						recallVerSensors();
@@ -580,7 +605,7 @@ task main()
 				}
 				recallHorSensors();
 
-				if (checkWin((int)colorYellow) == 1)
+				if (checkWin((int)colorYellow) == 1)		//Check for player win
 				{
 					win = 1;
 					displayWin(win);
@@ -588,32 +613,29 @@ task main()
 				}
 				else
 				{
-					driveAndDispense(columns, lastPieceRow, lastPieceCol);
-					recallVerSensors();
+					driveAndDispense(columns, lastPieceRow, lastPieceCol); 		// Make decision and dispense chip
+					recallVerSensors();																				// Reset robot position
 					recallHorSensors();
-					if (checkWin((int)colorRed) == 2)
+					if (checkWin((int)colorRed) == 2)			//Check for Robot win
 					{
 						win = 2;
 						displayWin(win);
 						wait1Msec(5000);
 					}
 				}
-				if (checkFullBoard())
+				if (checkFullBoard())				//Check for a full board i.e. a tie
 					win = 3;
 			}
-			endScreen();
+			endScreen(); // Display Play again
 			win = 0;
 			bool restart = false;
 			while(playAgain == true && !restart)
 			{
-				/*if (getButtonPress(buttonEnter))
-				playAgain = false;*/
-				if (getButtonPress(buttonEnter))
+				if (getButtonPress(buttonEnter))		// If pressed enter, end program
 				{
 					playAgain = false;
 				}
-
-				else if (SensorValue[S4])
+				else if (SensorValue[S4])		//If pressed touch sensor, play again
 				{
 					restart = true;
 					wait1Msec(3000);
